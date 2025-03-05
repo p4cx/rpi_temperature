d94dad8d31c3b6fae5ad32a3c04d6c547b626d57
p4cx <p4cx@pm.me>
2025-03-05 23:10:41 +0100

    Experiment with font

--- temperature.py
+++ temperature.py
@@ -5,7 +5,7 @@ import epaper
 import time
 from PIL import Image,ImageDraw,ImageFont
 
-font24 = ImageFont.truetype("./res/boxy-bold.ttf", 24)
+font24 = ImageFont.truetype("./res/boxy-bold.ttf", 28)
 
 epd = epaper.epaper('epd2in13_V4').EPD()
 epd.init()
@@ -17,6 +17,9 @@ time_draw = ImageDraw.Draw(time_image)
 epd.displayPartBaseImage(epd.getbuffer(time_image))
 while (True):
 	time_draw.rectangle((20, 10, 100, 34), fill = 255)
-	time_draw.text((20, 20), time.strftime('%H:%M'), font = font24, fill = 0)
+	string_var=time.strftime('%H:%M')
+	string_var = " ".join(c for c in string_var)
+
+	time_draw.text((20, 20), string_var, font=font24, fill = 0, align="center")
 	epd.displayPartial(epd.getbuffer(time_image))
 
