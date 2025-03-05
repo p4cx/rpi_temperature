dcd690d5756f7afbde458bb8783f921f4375eb40
p4cx <p4cx@pm.me>
2025-03-05 23:31:03 +0100

    Experiment with font

--- temperature.py
+++ temperature.py
@@ -5,7 +5,7 @@ import epaper
 import time
 from PIL import Image,ImageDraw,ImageFont
 
-font24 = ImageFont.truetype("./res/boxy-bold.ttf", 24)
+font24 = ImageFont.truetype("./res/karmatic-arcade.regular.ttf", 20)
 
 epd = epaper.epaper('epd2in13_V4').EPD()
 epd.init()
